d3a24cef0e2855a1ba296660667ede508a36bf9a
Rodolfo Mora <rodolfomora97@gmail.com>
2024-02-06 11:28:34 -0600

    componente

--- src/index.js
+++ src/index.js
@@ -16,11 +16,28 @@ root.render(
 const root = ReactDOM.createRoot(document.getElementById('root')); //jsx
 root.render('hola')*/
 
-
+/*
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const Hola = () => <p>Hola mundo</p>
+const Hola = () => <p>Hola mundo</p>  //JSX
 root.render(<Hola />)
+*/
 
+
+/* componente Li se le pueden pasar atributos y para utilizarlos se le pasa en ({children, estado, casa, edad}) del atributo */
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const Li = ({ children, estado, casa, edad}) => 
+  <li> {children} Mora {estado}  </li>
+
+
+  /* se le puede pasar atributos al componente */
+const Hola = () => 
+  <ul>
+    <Li estado="Hola" casa="Sucre" edad="10"> Figaro</Li> 
+    <Li estado="Sucre">Hola</Li>
+    <Li  estado="10">Figaro</Li>
+  </ul>
+
+root.render(<Hola />)
 
 
 reportWebVitals();
