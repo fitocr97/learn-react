e192797fb83c9fd35264e74a19e8de6493b59dd2
Rodolfo Mora <rodolfomora97@gmail.com>
2024-02-07 12:28:07 -0600

    agregar componente desde array

--- src/button.js
+++ src/button.js
@@ -1,11 +1,28 @@
 import './button.css'
 
+const arreglo = [ 
+    "hola 1",
+    "hola 2",
+    "hola 3",
+]
 
 //componente buttom
 const Button = (props) => {
-    return ( 
-        <button {...props} className="btn"></button> 
-    )
+    const variable = false //atributo que puede venir props
+    if (variable) {
+        <h1>hola esto es otro componente</h1>
+    } else {
+        return (
+             //map devuelve otro arreglo, el elemento , key id, aqui es el porque es unico
+            <div>
+                
+                {arreglo.map(el => <p key={el}> {el} </p> )}
+                
+                <button {...props} className="btn"></button> 
+            </div>
+        )
+    }
+    
 }
 
 export default Button
