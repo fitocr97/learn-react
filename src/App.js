1c5a455f21869f5bec400cdb1a2784b81ed26ce3
Rodolfo Mora <rodolfomora97@gmail.com>
2024-02-07 11:09:48 -0600

    agregar estilos inline a componente en react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+/*
 const Li = ({ children}) => {
   return (
     <li className='ccs-class'> {children} </li>
@@ -14,5 +14,30 @@ function App() {
     </ul>
   );
 }
+*/
+
+/* agregando estilos a componentes inline */
+//puede ser funcion variable
+const estilo = (bg) => ({
+  backgroundColor: bg,
+  color:  '#ffff',
+  padding: '10px 15px',
+  margin: '10px 15px'  
+})
+
+const Li = ({ children}) => {
+  return (
+    <li style={estilo('#abd')}  className='ccs-class'> {children} </li> //se llama el estilo al componente
+  )
+}
+
+function App() {
+  return (
+    <ul style={estilo('#dba')} className='ul-css'>
+      <Li estado="activo"> valos de li</Li>
+    </ul>
+  );
+}
+
 
 export default App;
