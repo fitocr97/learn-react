aca7aea7751522ded7b702de57dce5bd90e36ec2
Rodolfo Mora <rodolfomora97@gmail.com>
2024-02-07 12:04:50 -0600

    reutilizando componente con estilo button

--- src/App.js
+++ src/App.js
@@ -1,66 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import './main.css';
-/*
-const Li = ({ children}) => {
-  return (
-    <li className='ccs-class'> {children} </li>
-  )
-}
+import Button from './button.js'
 
 function App() {
   return (
-    <ul className='ul-css'>
-      <Li estado="activo"> valos de li</Li>
-    </ul>
-  );
-}
-*/
-
-/* agregando estilos a componentes inline */
-//puede ser funcion variable
-/*
-const estilo = (bg) => ({
-  backgroundColor: bg,
-  color:  '#ffff',
-  padding: '10px 15px',
-  margin: '10px 15px'  
-})
-
-const Li = ({ children}) => {
-  return (
-    <li style={estilo('#abd')}  className='ccs-class'> {children} </li> //se llama el estilo al componente
-  )
-}
-
-function App() {
-  return (
-    <ul style={estilo('#dba')} className='ul-css'>
-      <Li estado="activo"> valos de li</Li>
-    </ul>
-  );
-}
-*/
-
-//usar css desde un archivo css import './main.css';
-const estilo = (bg) => ({
-  backgroundColor: bg,
-  color:  '#ffff',
-  padding: '10px 15px',
-  margin: '10px 15px'  
-})
-
-const Li = ({ children}) => {
-  return (
-    <li className='clase-li'> {children} </li> //se llama el estilo al componente
-  )
-}
-
-function App() {
-  return (
-    <ul className='clase-li'>
-      <Li estado="activo"> valos de li</Li>
-    </ul>
+    <div>
+      <h1>Hola</h1>
+      <Button onClick={() => console.log('click')}>Click</Button>
+    </div>
+    
   );
 }
 
---
+++ src/button.js
@@ -0,0 +1,11 @@
+import './button.css'
+
+
+//componente buttom
+const Button = (props) => {
+    return ( 
+        <button {...props} className="btn"></button> 
+    )
+}
+
+export default Button
